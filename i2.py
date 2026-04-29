6b0a2579821cbc1b4c1cb55b91e6bc01c289761e
Fpyy <medeiros.bernardo10@gmail.com>
2026-04-29 15:39:53 -0300

    fox

--- i2.py
+++ i2.py
@@ -1,41 +1,26 @@
-'''
-Desafio de Programação: Gestor de Notas
-Desenvolva um programa que permita o cadastro de diversos alunos e suas respectivas notas.
- O programa deve funcionar da seguinte forma:
-Entrada de Dados: O programa deve solicitar o nome do aluno (ou a nota). Para encerrar 
-a entrada de dados, o usuário deve digitar "fim".
-Processamento Individual: Para cada aluno cadastrado, o programa deve solicitar as notas
- (ex: Nota 1 e Nota 2) e exibir imediatamente a média desse aluno.
-Acúmulo de Dados: O programa deve manter o controle interno de:
-Quantos alunos foram processados.
-A soma das médias de todos os alunos para calcular a média global.
-Encerramento: Ao digitar "fim", o programa deve exibir um relatório final contendo:
-O total de alunos cadastrados.
-A média geral da turma.
-'''
-total_alunos = 0
-soma_medias = 0
+totalalunos = 0
+somamedias = 0
 
 while True:
-    nome = input("Digite o nome do aluno (digite 'fim' para encerrar): ")
+    nome = input("Qual o nome do aluno: (fim para parar) ")
     
     if nome == "fim":
         break
 
-    nota1 = float(input("Digite a Nota 1: "))
-    nota2 = float(input("Digite a Nota 2: "))
+    nota1 = float(input("Digite uma nota: "))
+    nota2 = float(input("Digite a outra nota "))
 
     media = (nota1 + nota2) / 2
     print(f"Média de {nome}: {media:.2f}")
 
-    total_alunos += 1
-    soma_medias += media
+    totalalunos += 1
+    somamedias += media
 
-print("--- RELATÓRIO FINAL ---")
-print(f"Total de alunos: {total_alunos}")
+print("- RELATÓRIO FINAL -")
+print(f"Total de alunos: {totalalunos}")
 
-if total_alunos > 0:
-    media_geral = soma_medias / total_alunos
-    print(f"Média geral da turma: {media_geral:.2f}")
+if totalalunos > 0:
+    media = somamedias / totalalunos
+    print(f"Média da turma: {media:.2f}")
 else:
-    print("Nenhum aluno foi cadastrado.")
+    print("Sem alunos cadastrados")
